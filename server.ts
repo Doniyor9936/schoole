80615bc32536e17bd4d19b97154a1af485f59e83
Doniyor Qalandarov <doniyorqalandarov853@gmail.com>
2025-03-02 17:04:26 +0500

    add

--- server.ts
+++ server.ts
@@ -2,16 +2,37 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import userRouter from "./router/userRouter";
-import studentRouter  from "./router/studentRouter";
+import studentRouter from "./router/studentRouter";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "School API",
+      version: "1.0.0",
+      description: "School management system API",
+    },
+    servers: [
+      {
+        url: `http://localhost:${PORT}`,
+      },
+    ],
+  },
+  apis: ["./routes/*.ts"], // root papkada bo‘lsa
+};
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(userRouter)
-app.use(studentRouter)
+app.use(userRouter);
+app.use(studentRouter);
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(PORT, () => {
   console.log(`server running:${PORT}`);
